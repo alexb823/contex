d16ae60c1b7ced055104cc2994f34d16a09ca89d
Alex Bukhman <alexb823@gmail.com>
2019-10-20 19:21:12 -0400

    initial commit. created navbar and navbar styles

---
+++ src/Navbar.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  InputBase,
+  Switch,
+} from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import { Search as SearchIcon } from '@material-ui/icons';
+import styles from './styles/NavBarStyles';
+
+class Navbar extends Component {
+  render() {
+    const {classes} = this.props;
+    return (
+      <div className={classes.root}>
+        <AppBar position="static" color="primary">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit">
+              <span role="img">🇫🇷</span>
+            </IconButton>
+            <Typography className={classes.title} variant="h6" color="inherit">
+              App Title
+            </Typography>
+            <Switch />
+            <div className={classes.grow} />
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase placeholder="Search..." classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput
+              }} />
+            </div>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(Navbar);
