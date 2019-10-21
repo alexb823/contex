f294a2523e7bef532c0a569189bbb8ff59ccc24f
EC2 Default User <ec2-user@ip-172-31-24-220.us-east-2.compute.internal>
2019-10-21 19:46:34 +0000

    initial commit

---
+++ src/Form.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Avatar, Button, CssBaseline, FormControl, FormControlLabel, Checkbox, Input, InputLabel, Paper, Typography, MenuItem, Select } from '@material-ui/core';
+import { LockOutlined as LockOutlinedIcon } from '@material-ui/icons';
+import { withStyles } from '@material-ui/core/styles';
+
+class Form extends Component {
+  render() {
+    return <h1>hi</h1>
+  }
+}
+
+export default Form;
