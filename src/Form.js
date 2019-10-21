2b4b167df2982ab62da4cf52ef8a12df85b6a0a6
EC2 Default User <ec2-user@ip-172-31-24-220.us-east-2.compute.internal>
2019-10-21 21:29:32 +0000

    started Form

--- src/Form.js
+++ src/Form.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import { Avatar, Button, CssBaseline, FormControl, FormControlLabel, Checkbox, Input, InputLabel, Paper, Typography, MenuItem, Select } from '@material-ui/core';
 import { LockOutlined as LockOutlinedIcon } from '@material-ui/icons';
 import { withStyles } from '@material-ui/core/styles';
+import styles from './styles/FormStyles';
 
 class Form extends Component {
   render() {
-    return <h1>hi</h1>
+    const { classes } = this.props;
+    return (
+      <main className={classes.main}>
+      <Paper className={classes.paper}>
+      <Avatar className={classes.avatar}>
+      <LockOutlinedIcon />
+      </Avatar>
+      <Typography variant="h5">Sign In</Typography>
+      <Select>
+      <MenuItem>English</MenuItem>
+      <MenuItem>French</MenuItem>
+      <MenuItem>Spanish</MenuItem>
+      </Select>
+      </Paper>
+      </main>
+    )
   }
 }
 
-export default Form;
+export default withStyles(styles)(Form);
